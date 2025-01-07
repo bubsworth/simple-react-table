5f6eea6f8046f64ca5c5c21b3d340515a0f777ef
bubsworth <bubbard96@hotmail.com>
2025-01-07 15:57:43 +0000

    Add model and styling

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 import "./App.css";
+import { Modal } from "./components/Modal";
 import { Table } from "./components/Table";
 
 function App() {
   return (
     <div className="App">
       <Table />
+      <Modal />
     </div>
   );
 }
---
+++ src/components/Modal.jsx
@@ -0,0 +1,32 @@
+import React from "react";
+import "./Modal.css";
+
+export const Modal = () => {
+  return (
+    <div className="modal-container">
+      <div className="modal">
+        <form>
+          <div className="form-group">
+            <label htmlFor="page">Page</label>
+            <input name="page" />
+          </div>
+          <div className="form-group">
+            <label htmlFor="description">Description</label>
+            <textarea name="description" />
+          </div>
+          <div className="form-group">
+            <label htmlFor="status">Status</label>
+            <select name="status">
+              <option value="live">Live</option>
+              <option value="draft">Draft</option>
+              <option value="error">Error</option>
+            </select>
+          </div>
+          <button type="submit" className="btn">
+            Submit
+          </button>
+        </form>
+      </div>
+    </div>
+  );
+};
