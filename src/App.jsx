71b44c9cdc0070f330f22e5cd5d4dd8e766c7e2b
bubsworth <bubbard96@hotmail.com>
2025-01-07 16:24:22 +0000

    Add delete row functionality

--- src/App.jsx
+++ src/App.jsx
@@ -6,14 +6,19 @@ import { Table } from "./components/Table";
 function App() {
   const [modalOpen, setModalOpen] = useState(false);
 
-  const rows = [
+  const [rows, setRows] = useState([
     { page: "Home", description: "This is the first page", status: "live" },
     { page: "Page 2", description: "This is the second page", status: "draft" },
     { page: "Page 3", description: "This is the third page", status: "error" },
-  ];
+  ]);
+
+  const handleDeleteRow = (targetIndex) => {
+    setRows(rows.filter((__, idx) => idx !== targetIndex));
+  };
+
   return (
     <div className="App">
-      <Table rows={rows} />
+      <Table rows={rows} deleteRow={handleDeleteRow} />
       <button className="btn" onClick={() => setModalOpen(true)}>
         Add
       </button>
